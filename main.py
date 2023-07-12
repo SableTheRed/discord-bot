2ad508d1c4a4bb791867608270c8fa6068c3486e
SableTheRed <isobel@audioengineer.co.uk>
2023-07-13 00:45:33 +0100

    nevermind i couldn't get that to work

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import discord
 from discord.ext import commands
 from settings import token
 
-client = commands.Bot(command_prefix = ".")
+client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
     print(f"Logged into discord api as: {client.user}") # logged into discord api as: user#0001
 
-@client.command()
-async def ping(ctx): # ctx stands for context
-    await ctx.send("Pong")
+@client.slash_command()
+async def ping(ctx):
+    await ctx.respond("Pong")
 
 
 
