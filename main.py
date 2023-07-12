77b855aac8dfb71df84ce1b11453204985d87129
SableTheRed <isobel@audioengineer.co.uk>
2023-07-13 00:24:22 +0100

    does the cute little ping -> pong thing

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import discord
 from discord.ext import commands
+from settings import token
 
 client = commands.Bot(command_prefix = ".")
 
 @client.event
 async def on_ready():
     print(f"Logged into discord api as: {client.user}") # logged into discord api as: user#0001
+
+@client.command()
+async def ping(ctx): # ctx stands for context
+    await ctx.send("Pong")
+
+
+
+
+
+client.run(token)
